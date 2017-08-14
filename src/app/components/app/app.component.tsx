6a39c25b44a75e18d88dad8f649f2729fdd1448f
Paulius R <paulius.repsys@visma.com>
2017-08-14 09:26:58 +0300

    Made sidebar sticky

--- src/app/components/app/app.component.tsx
+++ src/app/components/app/app.component.tsx
@@ -4,22 +4,38 @@ import { AppStore } from '../shared/app.store';
 import { NavComponent } from '../nav/nav.component';
 import { MatchesListComponent } from '../matches/matches-list/matches-list.component';
 import { MatchesFiltersComponent } from '../matches/matches-filters/matches-filters.component';
+import { StickyComponentProps } from './app.interfaces';
+const { StickyContainer, Sticky } = require('react-sticky');
 const styles = require('./app.styles.scss');
-
 const appStore = new AppStore();
 
 export const AppComponent = observer(() => {
+
+    const stickySidebarFn = (props: StickyComponentProps) => {
+        const style = props.calculatedHeight < window.innerHeight
+            ? props.style
+            : {};
+
+        return (
+            <div className={styles.sidebar} style={style}>
+                <MatchesFiltersComponent appStore={appStore} />
+            </div>
+        );
+    };
+
     return (
         <div>
             <NavComponent title={appStore.league}></NavComponent>
-            <div className={styles.container}>
-                <div className={styles.sidebar}>
-                    <MatchesFiltersComponent appStore={appStore} />
-                </div>
+
+            <StickyContainer className={styles.container}>
+                <Sticky disableCompensation={true}>
+                    {stickySidebarFn}
+                </Sticky>
                 <div className={styles.content}>
                     <MatchesListComponent appStore={appStore} />
                 </div>
-            </div>
+            </StickyContainer>
         </div>
     );
+
 });
---
+++ src/app/components/app/app.interfaces.ts
@@ -0,0 +1,6 @@
+export interface StickyComponentProps {
+    style: {
+        [key: string]: string;
+    };
+    calculatedHeight: number;
+}
